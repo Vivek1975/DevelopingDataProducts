168fc038d3a8bd10e5f7dddc72470ba3aa0cdff6
Vivek1975 <vivekindbusprof@gmail.com>
2017-02-03 11:30:08 +0530

    Add files via upload

---
+++ server.R
@@ -0,0 +1,36 @@
+#
+# This is the server logic of a Shiny web application. You can run the 
+# application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+generateReport <- function(par) {
+  out.markdown <- ''
+  withProgress(message = 'Generating Report',
+               value = 0, {
+                 out.markdown <- rmarkdown::render(
+                   input = par,
+                   output_format = "html_document")
+                 
+                 setProgress(1)
+               })
+  
+  readr::read_file(out.markdown)
+}
+
+shinyServer(function(input, output) {
+  
+  output$out1 <- renderText(as.numeric(input$box1) + as.numeric(input$box2))
+  output$out2 <- renderText(as.numeric(input$box1) - as.numeric(input$box2))
+  output$out3 <- renderText(as.numeric(input$box1) * as.numeric(input$box2))
+  output$out4 <- renderText(as.numeric(input$box1) / as.numeric(input$box2))
+  output$report <- renderText({ HTML(generateReport("Week4ProjectDocumentation.Rmd")) })
+  output$ui <- renderText({ HTML(generateReport("UI.Rmd")) })
+  output$server <- renderText({ HTML(generateReport("Server.Rmd")) })
+})
+
---
+++ ui.R
@@ -0,0 +1,39 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny) 
+
+shinyUI(fluidPage(
+  titlePanel("Tiny Calculator"),
+  sidebarLayout(
+    sidebarPanel(
+      textInput("box1", "Enter first number:", value = 0), 
+      textInput("box2", "Enter second number:", value = 0),
+      submitButton("Submit")
+     ),
+    mainPanel(
+      h3("Addition:"),
+      textOutput("out1"),
+      h3("Subtraction:"),
+      textOutput("out2"),
+      h3("Multiplication:"),
+      textOutput("out3"),
+      h3("Division:"),
+      textOutput("out4"),
+      tabsetPanel(
+        tabPanel("Application Documentation", htmlOutput("report")),
+        tabPanel("Shiny User Interface", htmlOutput("ui")), 
+        tabPanel("Shiny Server", htmlOutput("server"))
+      ),
+      h5("shinyUI and shinyServer Code", a("Link to Github", href="www.google.com")),
+      p(strong(em("Github repository:",a("Developing Data Products - Link to Code",href="https://github.com/CrazyFarang/DevelopingDataProducts"))))
+      )
+    )
+  )
+)
